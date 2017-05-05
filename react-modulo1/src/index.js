fe91850c4a5ab78c004e972d1c040b2232f6a9c9
Bruna Almeida <brunacaster@gmail.com>
2017-05-05 18:00:58 +0100

    Finalizado modulo 1 do curso react

--- react-modulo1/src/index.js
+++ react-modulo1/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import AutorBox from './Autor';
+import LivroBox from './Livro';
+import Home from './Home';
 import './index.css';
-import { BrowserRouter, Route, browserHistory } from 'react-router-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 ReactDOM.render(
-  (<BrowserRouter history={browserHistory}>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/autor" component={AutorBox} />
-      <Route path="/livro" />
-    </div>
-  </BrowserRouter>),
+  (<Router history={browserHistory}>
+      <Route path="/" component={App} >
+        <IndexRoute component={Home} />
+        <Route path="/autor" component={AutorBox} />
+        <Route path="/livro" component={LivroBox}/>
+      </Route>
+  </Router>),
   document.getElementById('root')
 );
